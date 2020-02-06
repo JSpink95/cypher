c91f486e56e64ae5a45bde5c20fbaded2e209fb4
jspink95 <jackspink95@gmail.com>
2020-02-06 17:19:00 +0000

    Add main menu bar for ENtityEditor

--- Apps/EntityEditor/src/EntityEditor.cpp
+++ Apps/EntityEditor/src/EntityEditor.cpp
@@ -67,56 +67,73 @@ void EntityEditorApplication::OnImGuiRender()
         return false;
     };
 
-    static char* componentIdBuffer = new char[32]{ '\0' };
-    ImGui::InputText("Id", componentIdBuffer, 32);
-
-    static int selected = 0;
-    ImGui::Combo("##component_list", &selected, vectorStringGetter, &registeredComponentListNames, registeredComponentListNames.size());
-
-    ImGui::SameLine();
-    const bool addComponent = ImGui::Button("+##add_component");
-
-    if (addComponent && componentIdBuffer[0] != '\0')
+    ImGui::BeginMainMenuBar();
     {
-        TypeBase* componentType = registeredComponentList.at(selected);
-        Component* component = reinterpret_cast<Component*>(componentType->New());
-        component->SetOwner(entity.get());
-        component->SetId(componentIdBuffer);
-
-        Ref<Component> managed;
-        managed.reset(component);
-
-        entity->AddComponent(managed);
-
-        delete[] componentIdBuffer;
-        componentIdBuffer = new char[32]{ '\0' };
-    }
-
-    ImGui::Separator();
-
-    entity->ForEachComponent([](const ComponentId& id, Ref<Component> component) -> void
-    {
-        if (ImGui::TreeNode(id.GetStringId().c_str()))
+        if (ImGui::BeginMenu("File"))
         {
-            TypeBase* type = TypeRegister::GetRegisteredType(component->GetTypeName());
-            do {
-                const bool typeHasProperties = true;
-                if (typeHasProperties && ImGui::TreeNode(type->GetTypeName().c_str()))
-                {
-                    for (TypeBase::property_iterator it = type->property_begin(); it != type->property_end(); ++it)
+            const bool save = ImGui::MenuItem("Save...");
+            ImGui::EndMenu();
+        }
+    }
+    ImGui::EndMainMenuBar();
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 2.0f);
+    ImGui::SetNextWindowPos(ImVec2(4, 22));
+    if (ImGui::Begin("Entity", (bool*)0, ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoCollapse))
+    {
+        static char* componentIdBuffer = new char[32]{ '\0' };
+        ImGui::InputText("Id", componentIdBuffer, 32);
+
+        static int selected = 0;
+        ImGui::Combo("##component_list", &selected, vectorStringGetter, &registeredComponentListNames, registeredComponentListNames.size());
+
+        ImGui::SameLine();
+        const bool addComponent = ImGui::Button("+##add_component");
+
+        if (addComponent && componentIdBuffer[0] != '\0')
+        {
+            TypeBase* componentType = registeredComponentList.at(selected);
+            Component* component = reinterpret_cast<Component*>(componentType->New());
+            component->SetOwner(entity.get());
+            component->SetId(componentIdBuffer);
+
+            Ref<Component> managed;
+            managed.reset(component);
+
+            entity->AddComponent(managed);
+
+            delete[] componentIdBuffer;
+            componentIdBuffer = new char[32]{ '\0' };
+        }
+
+        ImGui::Separator();
+
+        entity->ForEachComponent([](const ComponentId& id, Ref<Component> component) -> void
+        {
+            if (ImGui::TreeNode(id.GetStringId().c_str()))
+            {
+                TypeBase* type = TypeRegister::GetRegisteredType(component->GetTypeName());
+                do {
+                    const bool typeHasProperties = true;
+                    if (typeHasProperties && ImGui::TreeNode(type->GetTypeName().c_str()))
                     {
-                        it->second->DisplayEditBox(component.get());
+                        for (TypeBase::property_iterator it = type->property_begin(); it != type->property_end(); ++it)
+                        {
+                            it->second->DisplayEditBox(component.get());
+                        }
+
+                        ImGui::TreePop();
                     }
 
-                    ImGui::TreePop();
-                }
+                    type = type->GetBaseType();
+                } while (type != nullptr);
 
-                type = type->GetBaseType();
-            } while (type != nullptr);
-
-            ImGui::TreePop();
-        }
-    });
+                ImGui::TreePop();
+            }
+        });
+    }
+    ImGui::End();
+    ImGui::PopStyleVar();
 }
 
 //////////////////////////////////////////////////////////////////////////
